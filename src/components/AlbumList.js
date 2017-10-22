45265d36421541fdb22cbfdb4c3cfc66873470ef
Ryan Clarke <ryan.w.clarke@outlook.com>
2017-10-22 13:33:39 -0400

    initial commit

---
+++ src/components/AlbumList.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
+
+class AlbumList extends Component {
+
+  state = {
+    albums: []
+  };
+
+  componentWillMount() {
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => this.setState({ albums: response.data }))
+      .catch(error => console.log(error));
+  }
+
+  renderAlbums() {
+    return this.state.albums.map(album =>
+      <AlbumDetail key={album.title} album={album} /> //key should be obj.id
+    );
+  }
+
+  render() {
+
+    console.log(this.state);
+
+    return (
+      <View>
+        {this.renderAlbums()}
+      </View>
+    );
+  }
+}
+
+export default AlbumList;
